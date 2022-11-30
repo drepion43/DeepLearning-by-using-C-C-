7a4c97af85027bc179294b9a67655decdd8b4416
drepion43 <drepion43@gmail.com>
2022-11-30 22:08:41 +0900

    1130

---
+++ variable.h
@@ -0,0 +1,11 @@
+#ifndef _VARIABLE_
+#define _VARIABLE_
+
+#include <list>
+#include <random>
+#include <memory>
+#include <boost/intrusive_ptr.hpp>
+
+#include "cuMat.h"
+#include "cuMatSparse.h"
+
